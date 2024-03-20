587857238ae06743fef2b33fcba4e119dcd79d26
zwwxxxxx <126754383+1tcmarki@users.noreply.github.com>
2024-03-20 14:57:43 +0100

    0&1. feladat
    0&1. feladat kesz.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   
+
+
+
+    class Program
+    {
+        static int Osszegzes(string f)
+        {
+            int db = 0;
+
+            foreach (int item in f)
+            {
+                db++;
+            }
+
+
+
+            return db;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            string forras = "szoveg";
+
+            int Forras_szama = Osszegzes(forras);
+            Console.WriteLine($"{Forras_szama}");
+
+
+        }
+    }
+}
