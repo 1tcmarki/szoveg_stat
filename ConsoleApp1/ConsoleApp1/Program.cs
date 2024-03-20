51c215e1e0ff4fcaa9dc837c0a41a53e7c8e4c9b
zwwxxxxx <126754383+1tcmarki@users.noreply.github.com>
2024-03-20 15:14:31 +0100

    qq
    qq

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -25,17 +25,33 @@ namespace ConsoleApp1
 
             return db;
         }
+        static void Forditas(string f)
+        {
+            for (int i = f.Length-1; i > 0; i--)
+            {
+                Console.WriteLine(f[i]);
 
 
+            }
+        
+        
+
+        }
 
         static void Main(string[] args)
         {
+            //0. feladat
             string forras = "szoveg";
-
+            //1. feladat
             int Forras_szama = Osszegzes(forras);
-            Console.WriteLine($"{Forras_szama}");
+            Console.WriteLine($"{Forras_szama} betubol all");
+            //2. feladat
+            string forditott_szoveg = "a";
+            Console.WriteLine($"{forditott_szoveg}");
 
 
+            Console.WriteLine("gomb");
+            Console.ReadKey();
         }
     }
 }
